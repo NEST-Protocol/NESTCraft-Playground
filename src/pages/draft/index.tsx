d7eb1eb889e94efeebba333afc4dd85c84db1476
tunogya <tunogya@qq.com>
2023-06-27 19:01:00 +0800

    update

---
+++ src/pages/draft/index.tsx
@@ -0,0 +1,22 @@
+const Draft = () => {
+  return (
+    <main className={'h-screen w-screen flex flex-col relative'}>
+      <div className={'h-16 flex items-center justify-between bg-white px-4 font-bold border-b'}>
+        <div>
+          NEST Craft
+        </div>
+        <div>
+          Connect Wallet
+        </div>
+      </div>
+      <div className={''}>
+        Canvas
+      </div>
+      <div className={'absolute bottom-0 right-8 bg-white px-3 py-2 rounded-tl-xl rounded-tr-xl w-80 border font-bold'}>
+        Function
+      </div>
+    </main>
+  )
+}
+
+export default Draft
