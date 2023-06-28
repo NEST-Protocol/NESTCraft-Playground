a178a8a1f10967e5c4f2e2cc9c0a8e1c69ff41ce
tunogya <tunogya@qq.com>
2023-06-28 18:28:31 +0800

    update

--- src/pages/draft/index.tsx
+++ src/pages/draft/index.tsx
@@ -1,5 +1,5 @@
 import {Fragment, useState} from "react";
-import {Dialog, Listbox, Popover, Transition} from "@headlessui/react";
+import {Dialog, Listbox, Transition} from "@headlessui/react";
 import {CheckIcon, ChevronUpDownIcon} from '@heroicons/react/20/solid'
 import {XCircleIcon} from "@heroicons/react/24/solid";
 
@@ -18,65 +18,62 @@ type ExpressionSubItem = {
 
 const Functions = [
   {
-    'coefficient': 1,
-    'function': 'm1',
-    'argument': {
-      'type': 'token',
-      'name': 'token',
-      'value': 'ETH',
+    coefficient: 1,
+    function: 'm1',
+    argument: {
+      type: 'token',
+      name: 'token',
+      value: 'ETH',
+
     },
-    'description': 'Linear function: m1 = a * x + c',
+    description: 'Linear function: m1 = a * x + c',
   },
   {
-    'coefficient': 1,
-    'function': 'm2',
-    'argument': {
-      'type': 'token',
-      'name': 'token',
-      'value': 'ETH',
+    coefficient: 1,
+    function: 'm2',
+    argument: {
+      type: 'token',
+      name: 'token',
+      value: 'ETH',
     },
-    'description': 'Square function, m2 = a * x ** 2 + c',
+    description: 'Square function, m2 = a * x ** 2 + c',
   },
   {
-    'coefficient': 1,
-    'function': 'm3',
-    'argument': {
-      'type': 'token',
-      'name': 'token',
-      'value': 'ETH',
+    coefficient: 1,
+    function: 'm3',
+    argument: {
+      type: 'token',
+      name: 'token',
+      value: 'ETH',
     },
-    'description': 'Reciprocal function, m3 = a / x + c',
+    description: 'Reciprocal function, m3 = a / x + c',
   },
   {
-    'coefficient': 1,
-    'function': 'm4',
-    'argument': {
-      'type': 'token',
-      'name': 'token',
-      'value': 'ETH',
+    coefficient: 1,
+    function: 'm4',
+    argument: {
+      type: 'token',
+      name: 'token',
+      value: 'ETH',
     },
-    'description': 'Square root function, m4 = a * x ** 0.5 + c',
+    description: 'Square root function, m4 = a * x ** 0.5 + c',
   },
   {
-    'coefficient': 1,
-    'function': 'm5',
-    'argument': {
-      'type': 'token',
-      'name': 'token',
-      'value': 'ETH',
+    coefficient: 1,
+    function: 'm5',
+    argument: {
+      type: 'token',
+      name: 'token',
+      value: 'ETH',
     },
-    'description': 'logarithmic function, m5 = a * ln(x) + c',
+    description: 'logarithmic function, m5 = a * ln(x) + c',
   },
 ]
 
 const tokens = [
-  {value: 'ETH'},
-  {value: 'BTC'},
-  {value: 'BNB'},
-  {value: 'MATIC'},
-  {value: 'ADA'},
-  {value: 'DOGE'},
-  {value: 'XRP'},
+  {value: 'ETH', channelId: 0},
+  {value: 'BTC', channelId: 1},
+  {value: 'BNB', channelId: 2},
 ]
 
 const Draft = () => {
@@ -266,7 +263,7 @@ const Draft = () => {
           backgroundImage: 'radial-gradient(#bbb 5%, transparent 0)',
           backgroundSize: '40px 40px',
         }}
-        className={'z-0 w-full h-full bg-white flex flex-col justify-center items-center font-bold text-5xl'}>
+        className={'z-0 w-full h-full bg-white flex flex-col justify-center items-center font-bold text-5xl overflow-auto'}>
         <div className={'bg-white p-10 border rounded-xl shadow-sm flex gap-2 items-center'}>
           <div>
             =
