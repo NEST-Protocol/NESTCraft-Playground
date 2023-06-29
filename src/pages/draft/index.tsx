9beb7d7b9ffb0becdb18afb9db375da358d9568c
tunogya <tunogya@qq.com>
2023-06-29 18:30:07 +0800

    update

--- src/pages/draft/index.tsx
+++ src/pages/draft/index.tsx
@@ -16,70 +16,7 @@ import {InjectedConnector} from 'wagmi/connectors/injected';
 import {NEST_CRAFT_ABI} from "@/constant/abi";
 import {NEST_ADDRESS, NEST_CRAFT_ADDRESS} from "@/constant/address";
 import {bscTestnet} from "wagmi/chains";
-
-type ExpressionSubItem = {
-  coefficient: number
-  function: string
-  description: string
-  argument: {
-    type: string
-    name: string
-    value: string | number | undefined
-  } | null
-}
-
-const Functions: ExpressionSubItem[] = [
-  {
-    coefficient: 1,
-    function: 'm1',
-    argument: {
-      type: 'token',
-      name: 'ETH',
-      value: 0,
-    },
-    description: 'Linear function: m1 = a * x + c',
-  },
-  {
-    coefficient: 1,
-    function: 'm2',
-    argument: {
-      type: 'token',
-      name: 'ETH',
-      value: 0,
-    },
-    description: 'Square function, m2 = a * x ** 2 + c',
-  },
-  {
-    coefficient: 1,
-    function: 'm3',
-    argument: {
-      type: 'token',
-      name: 'ETH',
-      value: 0,
-    },
-    description: 'Reciprocal function, m3 = a / x + c',
-  },
-  {
-    coefficient: 1,
-    function: 'm4',
-    argument: {
-      type: 'token',
-      name: 'ETH',
-      value: 0,
-    },
-    description: 'Square root function, m4 = a * x ** 0.5 + c',
-  },
-  {
-    coefficient: 1,
-    function: 'm5',
-    argument: {
-      type: 'token',
-      name: 'ETH',
-      value: 0,
-    },
-    description: 'logarithmic function, m5 = a * ln(x) + c',
-  },
-]
+import {ExpressionSubItem, Functions} from "@/constant/functions";
 
 const tokens = [
   {name: 'ETH', value: 0},
